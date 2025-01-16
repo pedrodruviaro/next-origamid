1e6be73a8553b61805ed781e6719f94e5c036a51
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-16 08:40:31 -0300

    feat: metadata

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,5 +1,10 @@
 import { getCursos } from "@/api/cursos"
+import { Metadata } from "next"
 import Link from "next/link"
+
+export const metadata: Metadata = {
+  title: "Cursos",
+}
 
 export default async function CursosPage() {
   const cursos = await getCursos()
