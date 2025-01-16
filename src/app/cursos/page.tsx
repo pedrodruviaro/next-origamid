5963c7ea36368de6742df171286761e2f8fc8208
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-16 09:16:41 -0300

    feat: next image

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+import s from "./cursos.module.css"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -6,8 +8,17 @@ export const metadata: Metadata = {
 
 export default async function CursosPage() {
   return (
-    <div>
+    <div className={s["main-title"]}>
       <h1>Front end e UI Design</h1>
+      <Image
+        src="https://api.origamid.online/imagens/lobo_1.jpg"
+        width={1200} // use o valor original ou o valor máximo
+        height={800} // deve ser proporcional ao width, respeitando a proporção
+        alt="Lobo branco dormindo sobre pedras"
+        quality={80} // define a qualidade da imagem
+        sizes="100vw"
+        priority
+      />
     </div>
   )
 }
